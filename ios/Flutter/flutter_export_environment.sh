e744e4816a7d25a313bd44fb45f5633cda9d1757
Snir Yacoby <yacobysn@gmail.com>
2021-06-08 14:04:10 +0300

    Updated project to androidx and gradle.

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Code\FlutterSDK\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Code\personal_expense_app"
+export "FLUTTER_ROOT=E:\Code\Flutter SDK\flutter"
+export "FLUTTER_APPLICATION_PATH=K:\Code\PersonalExpenseApp"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\Code\FlutterSDK\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
