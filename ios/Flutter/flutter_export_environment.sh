540bb452f70de68a22a9949b223463f47adf7d70
Snirdude <yacobysn@gmail.com>
2021-07-03 17:00:34 +0300

    gitignore update

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=E:\Code\Flutter SDK\flutter"
-export "FLUTTER_APPLICATION_PATH=K:\Code\PersonalExpenseApp"
+export "FLUTTER_ROOT=C:\Code\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Code\PersonalExpenseApp"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
